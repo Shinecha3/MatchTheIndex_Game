981214d560adf2f22700aea520adb679aedea837
nnmei <kusumnipa.r@ku.th>
2025-10-15 08:30:01 +0700

    Add files via upload

---
+++ MatchTheIndex_Game-main/src/lib/cardClass/Factory/Card.java
@@ -0,0 +1,10 @@
+package lib.cardClass.Factory;
+
+import javax.swing.ImageIcon;
+
+public interface Card {
+    String getName();
+    ImageIcon getImage();
+    int getScore();
+    String getDescription();
+}
---
+++ MatchTheIndex_Game-main/src/main/App.java
@@ -0,0 +1,9 @@
+package main;
+
+import lib.User_Section.LoginGUI;
+
+public class App {
+    public static void main(String[] args) throws Exception{
+        new LoginGUI();
+    }
+}
