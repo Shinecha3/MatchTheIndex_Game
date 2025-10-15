d290895a6c462afd6031357fc951980593ea3419
Shinecha3 <218202319+Shinecha3@users.noreply.github.com>
2025-10-15 20:19:43 +0700

    เพิ่มรูป

--- src/lib/cardClass/Factory/CardSet.java
+++ src/lib/cardClass/Factory/CardSet.java
@@ -5,49 +5,50 @@ public class CardSet {
     public static String[] getSet(String setName) {
         switch (setName.toLowerCase()) {
 
-            case "imghorse":
-                return new String[]{
-                    "setC_applejack", "setC_fluttershy", "setC_glasses", "setC_perple",
-                    "setC_pink", "setC_pinkiepie", "setC_rainbow",
-                    "setC_rainbowdash"
-                };
-
-            case "imgslm":
-                return new String[]{
-                    "setB_angry", "setB_black", "setB_cople", "setB_eat",
-                    "setB_ice", "setB_wow", "setB_lov", "setB_kids"
-                };
-            
             case "imgfruit":
                 return new String[]{
-                    "setD_fruit1", "setD_fruit2", "setD_fruit3", "setD_fruit4",
-                    "setD_fruit5", "setD_fruit6", "setD_fruit7", "setD_fruit8"
+                    "setA_fruit1", "setA_fruit2", "setA_fruit3", "setA_fruit4",
+                    "setA_fruit5", "setA_fruit6", "setA_fruit7", "setA_fruit8"
+                };
+
+            case "imgfood":
+                return new String[]{
+                    "setA_food1", "setA_food2", "setA_food3", "setA_food4",
+                    "setA_food5", "setA_food6", "setA_food7", "setA_food8"
+                };
+            
+            case "imgdessert":
+                return new String[]{
+                    "setA_dessert1", "setA_dessert2", "setA_dessert3", "setA_dessert4",
+                    "setA_dessert5", "setA_dessert6", "setA_dessert7", "setA_dessert8"
+                };
+
+            case "imgdrink":
+                return new String[]{
+                    "setA_drink1", "setA_drink2", "setA_drink3", "setA_drink4",
+                    "setA_drink5", "setA_drink6", "setA_drink7", "setA_drink8"
                 };
 
             default: // img
                 return new String[]{
-                    "setA_duo", "setA_hand", "setA_heart", "setA_lego",
-                    "setA_princess", "setA_shine", "setA_smile", "setA_tail"
+                    "setA_food1", "setA_food2", "setA_food3", "setA_food4",
+                    "setA_food5", "setA_food6", "setA_food7", "setA_food8"
                 };
         }
     }
 
     public static String getBackImage(String setName) {
         switch (setName.toLowerCase()) {
-            case "imghorse":
-                return "rain.jpg";
-            case "imgslm":
-                return "huh.jpg";
             case "imgfruit":
-                return "setD_fruit0.jpg";
+                return "set_back0.jpg";
             default:
-                return "back.jpg";
+                return "set_back0.jpg";
         }
     }
 
     public static String getRandomCardSet() {
         String[] allSets = {
-                    "imgfruit", "imgfruit", "imgfruit"
+                    "imgfood", "imgdessert", "imgfruit", "imgdrink"
                 };
         int randIndex = (int) (Math.random() * allSets.length);
         System.err.println(new String(allSets[randIndex]));
